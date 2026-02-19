76e0bcca19ba020f114f6bbc3f35e378aaf0ae1f
apekshawadye7 <apekshawadye@gmail.com>
2026-02-19 13:32:30 +0530

    Update database connection method in constants.php
    Refactor database connection to use mysqli_init and SSL.

--- config/constants.php
+++ config/constants.php
@@ -1,17 +1,28 @@
-<?php 
-    //Start Session
-    session_start();
+<?php
 
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
-    //Create Constants to Store Non Repeating Values
-    define('SITEURL', 'http://localhost/food-ordering/');
-    define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
-    
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
-    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
+$conn = mysqli_init();
 
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+
+mysqli_real_connect(
+    $conn,
+    $host,
+    $user,
+    $pass,
+    $db,
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>
